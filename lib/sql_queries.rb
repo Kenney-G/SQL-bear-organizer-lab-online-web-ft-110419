1a8954ce5cf4e6572e2b26fa3e220f634965d9d9
Ken Goad <goadke@g.cofc.edu>
2019-12-31 13:28:05 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -41,11 +41,11 @@ DESC LIMIT 1"
 end
 
 def counts_number_of_bears_with_goofy_temperaments
-"SELECT color,
-COUNT(color)
+"SELECT temperament,
+COUNT(temperament)
 FROM bears
-GROUP BY color
-ORDER BY COUNT(color);"
+GROUP BY temperament
+ORDER BY COUNT("goofy");"
 end
 
 def selects_bear_that_killed_Tim
